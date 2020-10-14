e771b95f9391d573ba99a8b6a7e21b6ce2bc5555
@armywalrus <armywalrus@gmail.com>
2020-10-13 21:37:16 -0400

    added test code to run on local server

--- database.py
+++ database.py
@@ -1,28 +1,58 @@
+# from flask import Flask, render_template, redirect
+# import requests
+# from flask_pymongo import pymongo
+# import csv
+
+# app = Flask(__name__)
+
+# client = pymongo.MongoClient("mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority")
+# db = client.test
+
+# db.tech_stocks.drop()
+
+# with open('All_Stocks.csv') as csv_file:
+#         data = csv.reader(csv_file, delimiter=',')
+#         for row in data:
+#             stocks= {
+#                 "Stock Exchange": row[0],
+#                 "Stock": row[1],
+#                 "Stock Ticker": row[2],
+#                 "Date": row[3],
+#                 "Open": row[4],
+#                 "High": row[5],
+#                 "Low": row[6],
+#                 "Close": row[7],
+#                 "Adj Close": row[8],
+#                 "Volume": row[9]
+#             }
+#             db.tech_stocks.insert_one(stocks) 
+
 from flask import Flask, render_template, redirect
 import requests
-from flask_pymongo import pymongo
+from flask_pymongo import PyMongo
 import csv
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
 
-db.tech_stocks.drop()
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tech_stocks"
+mongo = PyMongo(app)
+mongo.db.tech_stocks.drop()
+
 
 with open('All_Stocks.csv') as csv_file:
-        data = csv.reader(csv_file, delimiter=',')
-        for row in data:
-            stocks= {
-                "Stock Exchange": row[0],
-                "Stock": row[1],
-                "Stock Ticker": row[2],
-                "Date": row[3],
-                "Open": row[4],
-                "High": row[5],
-                "Low": row[6],
-                "Close": row[7],
-                "Adj Close": row[8],
-                "Volume": row[9]
-            }
-            db.tech_stocks.insert_one(stocks) 
+    data = csv.reader(csv_file, delimiter=',')
+    for row in data:
+        stocks = {
+          "Stock Exchange": row[0],
+          "Stock": row[1],
+          "Stock Ticker": row[2],
+          "Date": row[3],
+          "Open": row[4],
+          "High": row[5],
+          "Low": row[6],
+          "Close": row[7],
+          "Adj Close": row[8],
+          "Volume": row[9],
+        }
+        mongo.db.tech_stocks.insert_one(stocks)
--- tech_app.py
+++ tech_app.py
@@ -1,19 +1,59 @@
+# from flask import Flask, render_template, redirect
+# import requests
+# from flask_pymongo import PyMongo
+# from flask import Flask, jsonify
+# import csv
+# import database
+# from flask_cors import CORS
+# import pymongo
+
+# # client = pymongo.MongoClient("mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority")
+# # db = client.test
+
+
+# app = Flask(__name__)
+# CORS(app)
+# app.config['MONGO_DBNAME'] = 'tech_stocks'
+# app.config["MONGO_URI"] = "mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority"
+# mongo = PyMongo(app)
+
+# @app.route("/tech_stocks")
+# def index():
+
+#     techStocks = mongo.db.tech_stocks
+#     output = []
+#     for s in techStocks.find():
+#         output.append({
+#             'Stock Exchange' : s['Stock Exchange'],
+#             'Stock' : s['Stock'],
+#             'Stock Ticker' : s['Stock Exchange'],
+#             'Date' : s['Date'],
+#             'Open' : s['Open'],
+#             'High' : s['High'],
+#             'Low' : s['Low'],
+#             'Close' : s['Close'],
+#             'Adj Close' : s['Adj Close'],
+#             'Volume' : s['Volume']
+#             })
+#     return jsonify({'result' : output})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, render_template, redirect
 import requests
 from flask_pymongo import PyMongo
 from flask import Flask, jsonify
 import csv
 import database
-
-import pymongo
-
-# client = pymongo.MongoClient("mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority")
-# db = client.test
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['MONGO_DBNAME'] = 'tech_stocks'
-app.config["MONGO_URI"] = "mongodb+srv://project_staff:walrus@cluster0.i8w5c.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tech_stocks"
 mongo = PyMongo(app)
 
 @app.route("/tech_stocks")
@@ -39,3 +79,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
